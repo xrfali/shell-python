73cf90a89a6c7ab3c1dbc58fcd1f69048e29d478
Ali <22730931+xrfali@users.noreply.github.com>
2026-05-18 23:33:23 +0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ from app.shell.parser import parse_args
 from app.shell.jobs import process_jobs_command, reap_bg_jobs, next_job_number, is_bg_job, Job
 from app.shell.completer import is_registred_completer, find_executable_paths, auto_complete, BUILT_INS
 from app.shell.io_utils import write_output_to_file, redirect_write, clear_redirect
-
+from app.shell.history import read_history_from_file, write_history_to_file
 from app.shell_context import ShellContext
 
 def process_complete_command(args, argl, ctx: ShellContext):
@@ -59,27 +59,6 @@ def process_cd_command(arg):
             os.chdir(arg)
     except FileNotFoundError:
         print(f"cd: {arg}: No such file or directory")
-
-def read_history_from_file(path, ctx: ShellContext):
-    commands_history = ctx.history
-    try:
-        with open(path, 'r') as f:
-            h = f.read().splitlines()
-            commands_history += h
-    except FileNotFoundError:
-        pass
-
-def write_history_to_file(path, ctx: ShellContext, op = 'w'):
-    commands_history = ctx.history
-    last_history_idx = ctx.last_history_idx
-
-    cmd_h = commands_history if last_history_idx == None else commands_history[last_history_idx:]
-    ctx.last_history_idx = len(commands_history)
-    try:
-        with open(path, op) as f:
-            f.writelines(cmd + '\n' for cmd in cmd_h)
-    except FileNotFoundError:
-        pass
 
 def process_vars_with_braces(arg, ctx):
     def check_patter(match):
