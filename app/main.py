c20bbf877a8efc2f0c7ea3eeda0de7dbf0da01fd
Ali <22730931+xrfali@users.noreply.github.com>
2026-05-18 22:17:24 +0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -2,80 +2,18 @@ import os
 import subprocess
 import readline
 import sys
-from typing import List, Dict
-from dataclasses import dataclass
+from typing import List
 import re
 from app.shell.parser import parse_args
+from app.shell.jobs import process_jobs_command, reap_bg_jobs, next_job_number, is_bg_job, Job
 
 from app.shell_context import ShellContext
-
-@dataclass
-class Job:
-    job_no: int
-    pid: int
-    cmd: str
-    status: str
-    process: subprocess.Popen
 
 
 BUILT_INS = ['echo', 'exit', 'type', 'pwd', 'complete', 'jobs', 'history', 'declare']
 
 matches = []
 lcp = ""
-
-
-def process_jobs_command(args, argl, ctx: ShellContext):
-
-    to_remove = []
-    jobs = ctx.jobs
-
-    for idx, job in enumerate(jobs):
-        marker = ' '
-        if idx == len(jobs) - 1:
-            marker = '+'
-        elif idx == len(jobs) - 2:
-            marker = '-'
-        
-        status = job.status
-        cmd = job.cmd
-
-        if job.process.poll() != None:
-            status = "Done"
-            cmd =  job.cmd[:-1]
-            to_remove.append(idx)
-        print(f"{[job.job_no]}{marker}  {status:<24}{cmd}")
-
-    for idx in reversed(to_remove):
-        jobs.pop(idx)
-    
-    return None
-
-def reap_bg_jobs(ctx: ShellContext):
-    jobs = ctx.jobs
-
-    to_remove = []
-    for idx, job in enumerate(jobs):
-        cmd = job.cmd
-
-        if job.process.poll() != None:
-            status = "Done"
-            cmd =  job.cmd[:-1]
-            to_remove.append(idx)
-            print(f"{[job.job_no]}+  {status:<24}{cmd}")
-    
-    for idx in reversed(to_remove):
-        jobs.pop(idx)
-
-def next_job_number(ctx: ShellContext) -> int:
-    jobs = ctx.jobs
-
-    job_nos = [job.job_no for job in jobs]
-    job_no = 1
-
-    while job_no in job_nos:
-        job_no += 1
-    
-    return job_no
 
 def is_registred_completer(command, ctx: ShellContext):
     completions = ctx.completions
@@ -253,12 +191,6 @@ def write_output_to_file(file_name, output, file_mode = 'w'):
     with open(file_name, file_mode) as file:
         output += '\n' if output else ''
         file.write(output)
-
-def is_bg_job(args):
-    if args[-1] == "&":
-        return args[:-1], True
-
-    return args, False
 
 def redirect_write(w):
     saved_stdout = os.dup(1)
